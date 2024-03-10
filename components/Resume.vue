d116b09e66638c3d257af42b2efc6d9c0fc8a938
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-10 12:20:11 -0300

    cria componente de resume

---
+++ components/Resume.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex flex-col items-center justify-center bg-base-200 rounded-3xl w-1/2 border-2 border-neutral">
+        <div class="w-20 h-20 bg-primary rounded-full"></div>
+        <h1 class="text-2xl font-bold mt-4 text-secondary">Enzo Fagundes</h1>
+        <p class="text-lg text-neutral-content">Full Stack Developer</p>
+        <div class="flex flex-col items-center mt-4">
+            <div class="flex items-center flex-col">
+                <span>
+                    <Icon name="ri:mail-fill" />
+                    <span class="ml-2">enzo.fagundes014@gmail.com</span>
+                </span>
+                <span>
+                    <Icon name="ri:phone-fill" />
+                    <span class="ml-2">+55 11 9 9999-9999</span>
+                </span>
+                <span>
+                    <Icon name="mdi:location" />
+                    <span class="ml-2">São Paulo, Brazil</span>
+                </span>
+            </div>
+            <a href="#" class="btn btn-primary mt-4">
+                Acesse meu currículo
+            </a>  
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+
+</script>
+
+<style scoped></style>
