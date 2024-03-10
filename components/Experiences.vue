cded08c20a8f9a56a8988594f5513ee58c1ec62d
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-10 12:19:47 -0300

    cria componente de experiences

---
+++ components/Experiences.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="flex flex-col items-center space-y-8 px-4 py-16 h-full">
+        <p class="text-center text-2xl">
+            Olá, tudo bem?
+        </p>
+        <p class="text-center">
+            Enzo Fagundes, é um brasileiro natural de Ribeirão Preto, São Paulo. 21 anos, estudante de Sistemas para Internet na FATEC Lins. Atualmente, sou estagiário em desenvolvimento web na empresa <a href="https://br.linkedin.com/company/sonni-tech/" target="_blank" class="text-primary">Sonnitech</a>.
+        </p>
+        <div>
+            <a class="btn btn-primary">
+                <Icon name="ri:file-list-3-fill" />
+                <span>
+                    Veja meu currículo
+                </span>
+            </a>
+        </div>
+    </div>
+</template>
