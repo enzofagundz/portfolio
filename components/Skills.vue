83ec19800ba02a2d935c5eadbfc3ec6d70dd515b
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-18 08:25:15 -0300

    Atualizações na página de habilidades

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="flex flex-col items-center space-y-8 justify-center px-4 py-16 h-full bg-base-200 rounded-3xl md:w-1/2 border-2 border-neutral">
-        <p class="text-center md:text-2xl text-sm">
+    <div class="flex flex-col items-center space-y-8 justify-center px-4 py-16 bg-base-200 rounded-3xl md:w-1/2 border-2 border-neutral">
+        <!-- <p class="text-center md:text-2xl text-sm">
             Enquanto desenvolvedor, tenho habilidades em diversas tecnologias e ferramentas. Veja abaixo algumas delas.
         </p>
-        <div class="space-y-4 flex flex-col items-center h-full">
+        <div>
             <ul class="grid lg:grid-cols-4 gap-4 grid-cols-2 md:grid-cols-3">
                 <li v-for="skill in skills" :key="skill.id" class="border border-secondary rounded-xl p-2 md:p-4 cursor-pointer hover:border-accent transition duration-500 ease-in-out hover:text-accent">
                     <span class="font-medium text-sm lg:text-lg">
@@ -11,7 +11,10 @@
                     </span>
                 </li>
             </ul>
-        </div>
+        </div> -->
+        <h1>
+            Em reforma
+        </h1>
     </div>
 </template>
 
