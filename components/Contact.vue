f9cfddd6ae04144ea2f1290d6e8794c1e058db01
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-20 07:41:17 -0300

    Adicionados componentes Contact.vue e Education.vue

---
+++ components/Contact.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="flex flex-col" :class="divAlignment">
+        <p class="text-sm md:text-base flex items-center" :class="textAlignment">
+            <Icon name="ri:mail-fill" />
+            <span>enzo.fagundes014@gmail.com</span>
+        </p>
+        <p class="text-sm md:text-base flex items-center" :class="textAlignment">
+            <Icon name="ri:phone-fill" />
+            <span>+55 11 9 9999-9999</span>
+        </p>
+        <p class="text-sm md:text-base flex items-center" :class="textAlignment">
+            <Icon name="mdi:location" />
+            <span>São Paulo, Brazil</span>
+        </p>
+    </div>
+</template>
+
+<script setup lang="ts">
+defineProps({
+    textAlignment: String,
+    divAlignment: {
+        type: String,
+        default: 'items-center'
+    }
+})
+</script>
+
+<style scoped>
+span {
+    @apply ml-2;
+}
+</style>
---
+++ components/Education.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="bg-base-200 rounded-2xl p-4 shadow-md w-1/2">
+        <div class="container space-y-4">
+            <h2 class="text-3xl font-semibold text-left text-primary">
+                <Icon name="mdi:book-open-page-variant" class="h-8 w-auto" />
+                <p>
+                    Minha formação
+                </p>
+            </h2>
+            <ul class="space-y-4 text-left md:text-lg ">
+                <li v-for="edu in education" :key="edu.id">
+                    <p class="font-semibold">
+                        {{ edu.course }} - {{ edu.institution }}
+                    </p>
+                    <p>
+                        {{ edu.period }}
+                    </p>
+                    <p v-if="edu.expectedConclusion">
+                        Previsão de conclusão: {{ edu.expectedConclusion }}
+                    </p>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script lang="ts" setup>
+const educationStore = useEducationStore()
+const education = educationStore.education
+</script>
