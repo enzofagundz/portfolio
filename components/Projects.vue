de1fae3ed144307df72177a0968093d4ccddb136
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-20 07:41:32 -0300

    Atualiza o layout do componente Projects.vue

--- components/Projects.vue
+++ components/Projects.vue
@@ -1,6 +1,31 @@
 <template>
-    <div>
-
+    <div class="space-y-2 flex flex-col md:flex-row-reverse md:space-y-0 md:space-x-2">
+        <div class="container md:w-1/3 flex md:justify-end">
+            <ul class="flex space-x-2">
+                <li>
+                    <div class="badge badge-primary">primary</div>
+                </li>
+                <li>
+                    <div class="badge badge-primary">primary</div>
+                </li>
+                <li>
+                    <div class="badge badge-primary">primary</div>
+                </li>
+            </ul>
+        </div>
+        <div class="container md:w-2/3">
+            <div class="card bg-base-200 border-2 border-neutral">
+                <div class="card-body">
+                    <h2 class="card-title">Card title!</h2>
+                    <p>If a dog chews shoes whose shoes does he choose?</p>
+                    <div class="card-actions justify-end">
+                        <button class="btn btn-primary btn-circle">
+                            <Icon name="mdi:open-in-new" />
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
