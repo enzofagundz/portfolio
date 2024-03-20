9a187f223376368066d38154fe6917257d366dcf
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-20 07:39:56 -0300

    Atualiza estilos e layout da seção de habilidades

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,20 +1,16 @@
 <template>
-    <div class="flex flex-col items-center space-y-8 justify-center px-4 py-16 bg-base-200 rounded-3xl md:w-1/2 border-2 border-neutral">
-        <!-- <p class="text-center md:text-2xl text-sm">
+    <div class="flex flex-col items-center space-y-8 justify-center px-4 py-16 bg-base-200 rounded-2xl md:w-1/2 shadow-md">
+        <p class="text-center lg:text-lg text-base">
             Enquanto desenvolvedor, tenho habilidades em diversas tecnologias e ferramentas. Veja abaixo algumas delas.
         </p>
         <div>
-            <ul class="grid lg:grid-cols-4 gap-4 grid-cols-2 md:grid-cols-3">
-                <li v-for="skill in skills" :key="skill.id" class="border border-secondary rounded-xl p-2 md:p-4 cursor-pointer hover:border-accent transition duration-500 ease-in-out hover:text-accent">
-                    <span class="font-medium text-sm lg:text-lg">
-                        {{ skill.name }}
-                    </span>
+            <ul class="grid lg:grid-cols-6 gap-4 grid-cols-4">
+                <li v-for="skill in skills" :key="skill.id" 
+                class="border border-primary rounded-xl p-2 lg:p-4 cursor-pointer hover:border-secondary transition duration-500 ease-in-out flex items-center justify-center text-primary hover:text-secondary">
+                    <Icon :name="skill.icon" class="w-6 h-6 lg:w-8 lg:h-8" /> 
                 </li>
             </ul>
-        </div> -->
-        <h1>
-            Em reforma
-        </h1>
+        </div>
     </div>
 </template>
 
