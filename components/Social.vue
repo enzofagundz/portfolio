62dd889ee44a58887cef0b3c4e7983196ce92cfd
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-21 20:11:20 -0300

    Adiciona atributo target="_blank" aos links das redes sociais

--- components/Social.vue
+++ components/Social.vue
@@ -6,7 +6,7 @@
         <div class="container">
             <ul class="space-y-4">
                 <li>
-                    <a href="https://www.linkedin.com/in/enzofagundz/" class="flex items-center hover:bg-base-200 rounded-lg py-6 md:px-2">
+                    <a href="https://www.linkedin.com/in/enzofagundz/" class="flex items-center hover:bg-base-200 rounded-lg py-6 md:px-2" target="_blank">
                         <div class="bg-neutral h-16 w-16 rounded-full flex items-center justify-center">
                             <Icon name="basil:linkedin-solid" class="w-8 h-8 md:w-12 md:h-12 text-base-100 dark:text-neutral-content" />
                         </div>
@@ -21,7 +21,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="https://www.github.com/enzofagundz/" class="flex items-center hover:bg-base-200 rounded-lg py-6 md:px-2">
+                    <a href="https://www.github.com/enzofagundz/" class="flex items-center hover:bg-base-200 rounded-lg py-6 md:px-2" target="_blank">
                         <div class="bg-neutral h-16 w-16 rounded-full flex items-center justify-center">
                             <Icon name="brandico:github" class="w-8 h-8 md:w-12 md:h-12 text-base-100 dark:text-neutral-content" />
                         </div>
