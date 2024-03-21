5f32c1bd77684713a3efbe6ecac9bf483159dc11
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-21 20:28:12 -0300

    Removidas as informações de email e número de telefone do componente de Contato

--- components/Contact.vue
+++ components/Contact.vue
@@ -1,15 +1,5 @@
 <template>
     <div class="flex flex-col" :class="divAlignment">
-        <p v-if="infos.email" class="text-sm md:text-base flex items-center" :class="textAlignment">
-            <Icon name="ri:mail-fill" />
-            <span>
-                {{ infos.email }}
-            </span>
-        </p>
-        <p class="text-sm md:text-base flex items-center" :class="textAlignment" v-if="infos.phoneNumber">
-            <Icon name="ri:phone-fill" />
-            <span>+55 {{ infos.phoneNumber }}</span>
-        </p>
         <p class="text-sm md:text-base flex items-center" :class="textAlignment">
             <Icon name="mdi:location" />
             <span>Sabino, São Paulo, Brazil</span>
@@ -24,21 +14,6 @@ defineProps({
         type: String,
         default: 'items-center'
     }
-})
-
-const infos = reactive({
-    phoneNumber: '',
-    email: ''
-}) as Info
-
-interface Info {
-    phoneNumber: string | undefined
-    email: string | undefined
-}
-
-onMounted(() => {
-    infos.phoneNumber = process.env.PHONE_NUMBER
-    infos.email = process.env.EMAIL
 })
 </script>
 
