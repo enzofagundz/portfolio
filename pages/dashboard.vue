b6a0a8b17fd158ffb52089a145f160d58e2b7b75
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-28 15:26:22 -0300

    Adiciona página de dashboard

---
+++ pages/dashboard.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h1>
+            Dashboard
+        </h1>
+    </div>
+</template>
+
+<script setup>
+definePageMeta({
+    title: 'Dashboard',
+    description: 'This is the dashboard page',
+})
+</script>
+
+<style></style>
