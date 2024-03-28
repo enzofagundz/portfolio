1e30d796ac33544b73025f9ce45712fb92ee810b
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-28 15:25:26 -0300

    Adicionado componente Alert.vue

---
+++ components/Alert.vue
@@ -0,0 +1,18 @@
+<template>
+    <div role="alert" class="alert max-w-xs" v-if="response?.value" :class="response?.value">
+        <Icon :name="response?.icon" class="w-6 h-6" />
+        <span>{{ response?.message }}</span>
+    </div>
+</template>
+
+<script lang="ts" setup>
+defineProps({
+    response: Object as PropType<{
+        value: string;
+        icon: string;
+        message: string;
+    }>
+})
+</script>
+
+<style></style>
