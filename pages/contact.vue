201bc57dbbb1ee5d90a729ec3d4e634e73a26d06
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-03-28 15:26:12 -0300

    Atualizações no formulário de contato

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,22 +3,24 @@
         <h1 class="text-2xl lg:text-4xl font-medium text-left text-neutral dark:text-base-content">
             Entre em contato
         </h1>
-        <div class="container flex justify-center">
-            <form class="space-y-4">
+        <div class="container flex justify-center items-center flex-col space-y-4">
+            <Alert :response="response" />
+            <form class="space-y-4" @submit.prevent="submit">
                 <label class="input input-bordered flex items-center gap-2">
                     Nome
-                    <input type="text" class="grow" name="name" />
+                    <input type="text" class="grow" name="name" v-model="name" />
                 </label>
                 <label class="input input-bordered flex items-center gap-2">
                     E-mail
-                    <input type="text" class="grow" name="email" />
+                    <input type="text" class="grow" name="email" v-model="email" />
                 </label>
                 <label class="flex items-center gap-2">
                     <textarea placeholder="Mensagem"
-                        class="textarea textarea-bordered textarea-lg w-full max-w-xs resize-none"></textarea>
+                        class="textarea textarea-bordered textarea-lg w-full max-w-xs resize-none"
+                        v-model="message"></textarea>
                 </label>
                 <div class="flex items-center justify-center">
-                    <button class="btn btn-primary btn-block">
+                    <button class="btn btn-primary btn-block" type="submit">
                         Enviar
                     </button>
                 </div>
@@ -28,6 +30,46 @@
 </template>
 
 <script lang="ts" setup>
+const name = ref('');
+const email = ref('');
+const message = ref('');
+const response = reactive({
+    value: '',
+    icon: '',
+    message: ''
+})
+
+watchEffect(() => {
+    if (response.value) {
+        name.value = '';
+        email.value = '';
+        message.value = '';
+    }
+})
+
+async function submit() {
+    try {
+        const { body, statusCode } = await $fetch('/api/contact', {
+            method: 'POST',
+            body: JSON.stringify({
+                name: name.value,
+                email: email.value,
+                message: message.value
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+
+        response.value = statusCode === 201 ? 'alert-success' : 'alert-error';
+        response.icon = statusCode === 201 ? 'mdi:check-circle' : 'mdi:alert-circle';
+        response.message = body.message;
+    } catch (error) {
+        response.value = 'alert-warning';
+        response.icon = 'mdi:alert-circle';
+        response.message = "Ocorreu um erro ao enviar a mensagem. Tente novamente mais tarde."
+    }
+}
 </script>
 
 <style scoped>
