b7ae9dcc0c4a88d06e21f6c7458fcde699855599
Enzo Fagundes <enzo.fagundes014@gmail.com>
2024-05-09 18:55:44 -0300

    WIP

--- pages/contact.vue
+++ pages/contact.vue
@@ -12,15 +12,16 @@
                 </label>
                 <label class="input input-bordered flex items-center gap-2">
                     E-mail
-                    <input type="text" class="grow" name="email" />
+                    <input type="text" class="grow" name="email" v-model="email" @blur="validateEmail"/>
                 </label>
                 <label class="flex items-center gap-2">
                     <textarea placeholder="Mensagem"
-                        class="textarea textarea-bordered textarea-lg w-full max-w-xs resize-none"></textarea>
+                        class="textarea textarea-bordered textarea-lg w-full max-w-xs resize-none"
+                        name="message"></textarea>
                 </label>
                 <input type="hidden" name="_gotcha">
                 <div class="flex items-center justify-center">
-                    <button class="btn btn-primary btn-block" type="submit">
+                    <button class="btn btn-primary btn-block" type="submit" :disabled="!emailValid">
                         Enviar
                     </button>
                 </div>
@@ -32,9 +33,20 @@
 <script lang="ts" setup>
 import { useRoute } from 'vue-router'
 import { ref } from 'vue'
+const email = ref('')
+const emailValid = ref(false)
 const route = useRoute()
 const success = ref(route.query.success ? true : false)
 const formsquashKey = ref(import.meta.env.VITE_FORMSQUASH_KEY)
+
+const validateEmail = () => {
+  const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
+  emailValid.value = regex.test(email.value);
+  if (!emailValid.value) {
+    alert('Por favor, insira um e-mail válido.');
+    email.value = '';
+  }
+};
 </script>
 
 <style scoped>
